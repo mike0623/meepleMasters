fad84adbe130ff6acca99649225e08f8b66338d4
Shoehorn <steven830815@gmail.com>
2023-04-12 18:10:25 +0800

    無更動
    上一版測試程式寫到一半忘記註解，為了美觀把半成品註解掉

--- src/main/java/tw/com/eeit162/meepleMasters/shoehorn/forum/service/ArticleService.java
+++ src/main/java/tw/com/eeit162/meepleMasters/shoehorn/forum/service/ArticleService.java
@@ -1,7 +1,5 @@
 package tw.com.eeit162.meepleMasters.shoehorn.forum.service;
 
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,14 +16,15 @@ public class ArticleService {
 		Article article1 = new Article();
 		article1.setArticleId(1);
 		article1.setFkProductId(1);
-		article1.setArticleComment(1);
-		
-		article1.setArticleContent(1);
-		article1.setArticleCreatedDate();
-		article1.setArticleUpdatedDate(1);
-		
+//		article1.setArticleComment(1);
+//		
+//		article1.setArticleContent(1);
+//		article1.setArticleCreatedDate();
+//		article1.setArticleUpdatedDate(1);
+//		
 		article1.setArticleTitle("test");
 		article1.setFkMemberId(1);
+		return article1;
 
 		
 		
