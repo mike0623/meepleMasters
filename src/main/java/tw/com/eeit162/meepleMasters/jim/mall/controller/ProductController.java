c3218d79c776cdcb3904e1fddb878a2563aaea83
仁敬のNB\Jim <Jim>
2023-04-17 13:33:48 +0800

    建立簡單的商城CRUD
    製作中4/17

--- src/main/java/tw/com/eeit162/meepleMasters/jim/mall/controller/ProductController.java
+++ src/main/java/tw/com/eeit162/meepleMasters/jim/mall/controller/ProductController.java
@@ -1,8 +1,31 @@
 package tw.com.eeit162.meepleMasters.jim.mall.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import tw.com.eeit162.meepleMasters.jim.mall.model.bean.Product;
+import tw.com.eeit162.meepleMasters.jim.mall.service.ProductService;
 
 @Controller
 public class ProductController {
 
+	@Autowired
+	private ProductService pService;
+
+	@GetMapping("/mall/findAllProduct")
+	@ResponseBody
+	public List<Product> findAllProduct() {
+		return pService.findAllProduct();
+	}
+
+	@GetMapping("/mall/findProductById/{id}")
+	@ResponseBody
+	public Product findProductById(@PathVariable(name = "id") Integer id) {
+		return pService.findProductById(id);
+	}
 }
--- src/main/java/tw/com/eeit162/meepleMasters/jim/mall/service/ProductService.java
+++ src/main/java/tw/com/eeit162/meepleMasters/jim/mall/service/ProductService.java
@@ -1,8 +1,11 @@
 package tw.com.eeit162.meepleMasters.jim.mall.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import tw.com.eeit162.meepleMasters.jim.mall.model.bean.Product;
 import tw.com.eeit162.meepleMasters.jim.mall.model.dao.ProductDAO;
 
 @Service
@@ -11,4 +14,19 @@ public class ProductService {
 	@Autowired
 	private ProductDAO pDAO;
 
+	public List<Product> findAllProduct() {
+		return pDAO.findAll();
+	}
+
+	public Product findProductById(Integer id) {
+		return pDAO.findById(id).get();
+	}
+
+	public void insertProduct(Product product) {
+		pDAO.save(product);
+	}
+
+	public void deleteProductById(Integer id) {
+		pDAO.deleteById(id);
+	}
 }
