efb7cae26f2870e4b898abaf765de54d02e59383
L <R>
2023-04-20 09:20:53 +0800

    新增卡片列表jsp
    4/20

--- src/main/java/tw/com/eeit162/meepleMasters/lyh/controller/CardListController.java
+++ src/main/java/tw/com/eeit162/meepleMasters/lyh/controller/CardListController.java
@@ -3,13 +3,17 @@ package tw.com.eeit162.meepleMasters.lyh.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import tw.com.eeit162.meepleMasters.lyh.model.bean.Card;
 import tw.com.eeit162.meepleMasters.lyh.model.bean.CardOwned;
 import tw.com.eeit162.meepleMasters.lyh.service.CardListService;
 
@@ -42,6 +46,19 @@ public class CardListController {
 		}
 		
 		return "沒有卡片";
+	}
+
+	@GetMapping("/downloadCard/{cardId}")
+	public ResponseEntity<byte[]> downloadCard(@PathVariable(name = "cardId") Integer cardId) {
+		
+		Card card = cListService.findById(cardId);
+		
+		byte[] cardFile = card.getCardImg();
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.IMAGE_JPEG);
+		
+		return new ResponseEntity<byte[]>(cardFile, headers, HttpStatus.CREATED);
 		
 	}
 
