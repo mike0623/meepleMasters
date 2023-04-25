bd3f1a0f26651602dcd0fb9111c2fd007797bc5c
Bling <blingw1031@gmail.com>
2023-04-25 10:33:39 +0800

    更改會員頭貼回傳型別
    4/25

--- src/main/java/tw/com/eeit162/meepleMasters/jack/controller/MemberController.java
+++ src/main/java/tw/com/eeit162/meepleMasters/jack/controller/MemberController.java
@@ -7,6 +7,10 @@ import javax.servlet.http.HttpSession;
 import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -186,9 +190,12 @@ public class MemberController {
 	 */
 	@GetMapping("/member/findMemberImg/{id}")
 	@ResponseBody
-	public byte[] findMemberImg(@PathVariable("id") Integer memberId) {
+	public ResponseEntity<byte[]> findMemberImg(@PathVariable("id") Integer memberId) {
+		byte[] memberImg = mService.findMemberImg(memberId);
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.IMAGE_PNG);
 		
-		return mService.findMemberImg(memberId);
+		return new ResponseEntity<byte[]>(memberImg, headers, HttpStatus.OK);
 	}
 	
 }
